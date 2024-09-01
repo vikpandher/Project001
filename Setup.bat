5cc9af7d7a847224b59c0f1d005e8488972ddbd1
Vik Pandher <vikpandher@gmail.com>
2024-09-01 12:16:22 -0500

    added setup script

---
+++ Setup.bat
@@ -0,0 +1,15 @@
+@ECHO off
+
+REM Initialize and update all submodules
+git submodule update --init --recursive
+
+REM Build the submodules
+CALL Scripts\Build_Vendors.bat
+
+CD %~dp0
+
+REM Set the build directory
+SET BUILD_DIR=Build
+
+REM Run CMake with the desired settings
+cmake -S . -B %BUILD_DIR% -G "Visual Studio 17 2022"
